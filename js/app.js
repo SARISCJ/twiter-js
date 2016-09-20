45ab8e21ec163d0b484d61354e4e2d7a6b9775d7
SARISCJ <saracastillo0@gmail.com>
2016-09-20 10:47:38 -0500

    cambiar de color

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function() {
+	var color = ["red","violet","purple"];
+	var maximo = [120,130,140];
     var boton = document.getElementById("enviar");
     boton.disabled = true;
     boton.addEventListener("click", function (e){
@@ -21,19 +23,30 @@ window.addEventListener("load", function() {
 		caracteres();
 	}) 
 
-	function caracteres () {
+	function caracteres (maximo, color) {
 		var texto = document.getElementById("texto");
 		var cantidadCaracteres = texto.value.length;
 		var limite = 140;
 		var contador = document.getElementById("contador");
 		contador.innerHTML = limite - cantidadCaracteres;
 
-		if (cantidadCaracteres >= limite) {
-			contador.removeClass("white").addClass("red");
-			boton.disabled = true;
-		} else {
-			contador.removeClass("red").addClass("blue");
-		}
+       if(largo >= maximo[0] && largo < maximo[1]) {  //largo >= 120 y largo < 130
+               contador.classList.remove(color[1]);
+                contador.classList.add(color[0]);
+       } else if(largo >= maximo[1] && largo < maximo[2]) {  //largo >= 130 y largo < 140
+               contador.classList.remove(color[0]);
+               contador.classList.remove(color[2]);
+               contador.classList.add(color[1]);    
+       } else if(largo >= maximo[2]){//largo >= 140
+               contador.classList.remove(color[1]);
+               contador.classList.add(color[2]);
+               boton.disabled = true;
+       } else {
+               for(var i = 0; i < maximo.length-1; i++){
+                   contador.classList.remove(color[i]);        
+               };
+       }
+
 	}
 });
 
